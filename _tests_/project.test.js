2ef1349431dcd3a59665d8d0126fe81a6b407cad
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-06-26 15:22:11 -0500

    add failing checkAge function test

--- _tests_/project.test.js
+++ _tests_/project.test.js
@@ -1,19 +1,26 @@
+import {checkAge}from './../src/inputChecker.js';
 import {userAge}from './../src/SolarYears.js';
 import { TestScheduler } from 'jest';
 
+
 describe('userAge', ()=>{
+  
 
   test('should correctly create a userAge class', ()=>{
     const user1 = new userAge(45);
     expect(user1.earthAge).toEqual(45);
   })
-  
-  test('Should check that a user enters number between 0 and 150', ()=>{
-    const tooOld = new userAge(151);
-    expect(tooOld.earthAge).toEqual('Please enter a number greater than 0 and less than 150');
-    const tooYoung = new userAge(0);
-    expect(tooYoung.earthAge).toEqual('Please enter a number greater than 0 and less than 150');
-  
-  });
 
+});
+
+
+describe('ageChecker', ()=>{
+
+test('Should check that a user enters number between 0 and 150', ()=>{
+  const tooOld = new userAge(151);
+  expect(tooOld.earthAge).toEqual(wrongAge);
+  const tooYoung = new userAge(0);
+  expect(tooYoung.earthAge).toEqual(wrongAge);
+
+});
 });
---
+++ src/inputChecker.js
@@ -0,0 +1,8 @@
+export function checkAge(userAge) {
+  // let wrongAge = "Please enter an age greater than 0 and less than 150";
+  // if(age<0 || age>150) {
+  //   return wrongAge;
+  // } else {
+  //   return true;
+  // }
+}
