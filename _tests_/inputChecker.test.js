9ffe1edae3b42ee5186f18bf5d797d63ec782569
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-07-08 11:09:24 -0500

    add a passing test for parameter that fits within inputChecker parameters

--- _tests_/inputChecker.test.js
+++ _tests_/inputChecker.test.js
@@ -4,7 +4,7 @@ describe("ageChecker", () => {
   test("Should check that a user enters number between 0 and 150", () => {
     expect(checkAge(155)).toEqual(true);
     expect(checkAge(-5)).toEqual(true);
-    expect(checkAge(5)).toEqual();
+    expect(checkAge(5)).toEqual(false);
   });
 
 });
