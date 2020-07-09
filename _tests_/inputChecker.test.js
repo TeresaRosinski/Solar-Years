465d4760f12eac98baf3d847823d9d030c62eaf1
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-07-09 10:16:33 -0500

    add a seperate test for values outside vs. inside years declared in inputChecker

--- _tests_/inputChecker.test.js
+++ _tests_/inputChecker.test.js
@@ -1,10 +1,13 @@
 import { checkAge } from "../src/inputChecker.js";
 
 describe("ageChecker", () => {
-  test("Should check that a user enters number between 0 and 150", () => {
-    expect(checkAge(155)).toEqual(true);
-    expect(checkAge(-5)).toEqual(true);
+  test("Should check that a user enters correctly number between 0 and 150", () => {
     expect(checkAge(5)).toEqual(false);
   });
-
+  test("Should check that a user enters a number outside of 0 and 150", () => {
+    expect(checkAge(155)).toEqual(true);
+    expect(checkAge(-5)).toEqual(true);
+   
+  });
+  
 });
